27522007803619dfbe34a078243cf650d14f58a1
Matthew Ong <matt.ong@hotmail.com>
2023-02-27 23:25:31 +0800

    astar - added neighbour check

--- include/astar/a-star-search.hpp
+++ include/astar/a-star-search.hpp
@@ -1,6 +1,8 @@
 #ifndef ASTAR_A_STAR_SEARCH_HPP
 #define ASTAR_A_STAR_SEARCH_HPP
 
+#include <unordered_set>
+
 #include "astar/common.hpp"
 #include "graph.hpp"
 
@@ -15,10 +17,23 @@ class AStarSearch {
   Path FindPath();
 
  private:
+  // Functions
+  void collate_walls_();
+  bool is_passable_(const Position pos);
+  std::vector<Position> neighbours(const Position grid_pos);
+
+  // Variables
   cv::Mat* graph_;
+  Position grid_size_;
   Position start_;
   Position goal_;
   Path path_;
+  std::unordered_set<Position> walls_;
+
+  // Constants
+  const double SQRT2 = 1.4142135623731;
+  const Position DIRS[8] = {{1, 0},  {1, 1},   {0, 1},  {-1, 1},
+                            {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
 };
 
 #endif /* ASTAR_A_STAR_SEARCH_HPP */
--- src/a-star-search.cpp
+++ src/a-star-search.cpp
@@ -8,6 +8,7 @@ AStarSearch::AStarSearch(cv::Mat* graph, Position start, Position goal) {
   graph_ = graph;
   start_ = start;
   goal_ = goal;
+  grid_size_ = Position(graph_->size().width, graph_->size().height);
 }
 
 void AStarSearch::UpdateGraph(cv::Mat* graph) { graph_ = graph; }
@@ -17,8 +18,36 @@ void AStarSearch::UpdateSearch(Position start, Position goal) {
   goal_ = goal;
 }
 
-Path AStarSearch::FindPath(){
-    // A star algorithm
-    
-    return path_;
+Path AStarSearch::FindPath() {
+  // A star algorithm
+
+  return path_;
+}
+
+std::vector<Position> AStarSearch::neighbours(Position grid_pos) {
+  std::vector<Position> neighbours;
+  for (int8_t i = 0; i < 8; i++) {
+    Position next(grid_pos + DIRS[i]);
+    if (is_passable_(next) && next < grid_size_) {
+      neighbours.push_back(next);
+    }
+  }
+}
+
+/**
+ * @brief Check for walls
+ *
+ * @param pos
+ * @return true
+ * @return false
+ */
+bool AStarSearch::is_passable_(const Position pos) {
+  return walls_.find(pos) == walls_.end();
+}
+
+void AStarSearch::collate_walls_() {
+  for(uint16_t i=0; i<graph_->rows; i++)
+    for(uint16_t j=0; j<graph_->cols; j++){
+      walls_.insert(Position(i,j));
+    }
 }
